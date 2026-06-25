4e4111b68a09b29d9013232312096fbf936db70e
Farhan Fadaei <farhanfadayi@gmail.com>
2026-06-25 22:04:44 +0430

    add force-dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import Hero from "@/components/Hero";
 import { getArticles } from "@/lib/services/article.service";
 import ArticleList from "@/components/ArticleList";
 
 export default async function Home() {
   const articles = await getArticles();
-
   return (
     <main>
       <Hero />
