ec7f07be3dd1006749a9a486b5594afe2dd48487
Farhan Fadaei <farhanfadayi@gmail.com>
2026-06-27 18:49:36 +0430

    article.url instead of index in ArticleList.tsx

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -10,9 +10,9 @@ export default function ArticleList({
 }: ArticleListProps) {
     return (
         <section className="px-4 py-10 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3">
-            {articles.map((article, index) => (
+            {articles.map((article) => (
                 <ArticleCard
-                    key={index}
+                    key={article.url}
                     article={article}
                 />
             ))}
